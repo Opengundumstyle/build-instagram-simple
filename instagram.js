367625f256fa8748399c274a8b01adbee19a9e4e
aymak91 <amakyu@gmail.com>
2022-09-29 21:56:49 -0700

    instagram.js

---
+++ instagram.js
@@ -0,0 +1,22 @@
+class Instagram {
+    constructor() {
+        // Write code here...
+    }
+
+    postPhoto(userId, photoId) {
+        // Write code here...
+    }
+
+    getFeed(userId) {
+        // Write code here...
+    }
+
+    follow(followerId, followeeId) {
+        // Write code here..
+    }
+
+    unfollow(followerId, followeeId) {
+        // Write code here..
+    }
+
+}
